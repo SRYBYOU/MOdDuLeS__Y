da1fd86daffbfad0cf9be24e0a599f5049c247fe
SRYBYOU <bloskmango@gmail.com>
2025-04-21 15:11:51 +0300

    Create KeYsS.lua

---
+++ FBI/KeYsS.lua
@@ -0,0 +1,11 @@
+return {
+    FreeKeys = {
+        "YHUBxFREE-2025-EOQhC9gF"
+    },
+    PremiumKeys = {
+        "YHUBxPREMIUM-2025-QXr9Zk7Lp3"
+    },
+    VIPKeys = {
+        "YHUBxVIP-2025-YFDQMASrE"
+    }
+}
