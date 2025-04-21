55bed2d9f307275f2d274d49409f321d3776098c
SRYBYOU <bloskmango@gmail.com>
2025-04-21 17:18:43 +0300

    Update KeYsS.lua

--- FBI/KeYsS.lua
+++ FBI/KeYsS.lua
@@ -3,9 +3,9 @@ return {
         { key = "YHUBxFREE-2025-EOQhC9gF", expirationDate = os.time{year = 2025, month = 4, day = 22, hour = 1, min = 1, sec = 1} },
     },
     PremiumKeys = {
-        { key = "YHUBxPREMIUM-2025-QXr9Zk7Lp3", expirationDate = os.time{year = 2025, month = 4, day = 30, hour = 0, min = 0, sec = 0} },
+        { key = "YHUBxPREMIUM-2025-QXr9Zk7Lp3" }, 
     },
     VIPKeys = {
-        { key = "YHUBxVIP-2025-YFDQMASrE", expirationDate = os.time{year = 9999, month = 1, day = 1, hour = 1, min = 1, sec = 1} },
+        { key = "YHUBxVIP-2025-YFDQMASrE" },  
     }
 }
