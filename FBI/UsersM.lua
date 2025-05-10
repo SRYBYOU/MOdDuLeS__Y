d568ac313c4acf25824221230bec98dae302b303
SRYBYOU <bloskmango@gmail.com>
2025-05-10 22:18:04 +0300

    Update UsersM.lua

--- FBI/UsersM.lua
+++ FBI/UsersM.lua
@@ -1,20 +1,31 @@
 return {
-  -- Dev list
-    Developerlist = {
-        ["Youssef_14444888"] = true
-    },
-  -- ranks
-    Ranks = {
-        ["Youssef_14444888"] = "Dev",
-        ["SDRO10P"] = "Distinct",
-        ["Fahad_1161"] = "Distinct"
-    },
-  -- vip users
-    VIPMembers = {
-        "Youssef_14444888"
-    },
-  -- PRM users
-    PremiumMembers = {
-        ""
-    }
+  Developerlist = {
+    ["Youssef_14444888"] = true
+  },
+
+  VIPMembers = {
+    "Youssef_14444888"
+  },
+
+  PremiumMembers = {
+    "Youssef_14444888"
+  },
+
+  Ranks = {
+    ["Youssef_14444888"] = "Dev",
+    ["SDRO10P"] = "Distinct",
+    ["Fahad_1161"] = "Distinct"
+  }
 }
+
+for _, member in pairs(VIPMembers) do
+    if not Ranks[member] then
+        Ranks[member] = "VIP"
+    end
+end
+
+for _, member in pairs(PremiumMembers) do
+    if not Ranks[member] then
+        Ranks[member] = "Premium"
+    end
+end
