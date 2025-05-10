d48b5839f834a75f6a09ce1aa3717ff71243db2c
SRYBYOU <bloskmango@gmail.com>
2025-05-10 13:04:47 +0300

    Create UsersM.lua

---
+++ FBI/UsersM.lua
@@ -0,0 +1,20 @@
+return {
+  -- Dev list
+    Developerlist = {
+        ["Youssef_14444888"] = true
+    },
+  -- ranks
+    Ranks = {
+        ["Youssef_14444888"] = "Dev",
+        ["SDRO10P"] = "Distinct",
+        ["Fahad_1161"] = "Distinct"
+    },
+  -- vip users
+    VIPMembers = {
+        "Youssef_14444888"
+    },
+  -- PRM users
+    PremiumMembers = {
+        ""
+    }
+}
