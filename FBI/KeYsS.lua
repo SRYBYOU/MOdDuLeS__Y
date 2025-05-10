d09b485b10defb617f655bda95af02ba06e1c0ed
SRYBYOU <bloskmango@gmail.com>
2025-05-10 18:29:09 +0300

    Update KeYsS.lua

--- FBI/KeYsS.lua
+++ FBI/KeYsS.lua
@@ -1,6 +1,6 @@
 return {
     FreeKeys = {
-        { key = "YHUBxFREE-2025-EOQhC9gF", expirationDate = os.time{year = 2025, month = 5, day = 3, hour = 1, min = 1, sec = 1} },
+        { key = "YHUBxFREE-2025-EOQhC9gF", expirationDate = os.time{year = 2025, month = 6, day = 3, hour = 1, min = 1, sec = 1} },
     },
     PremiumKeys = {
         "YHUBxPREMIUM-2025-QXr9Zk7Lp3",
