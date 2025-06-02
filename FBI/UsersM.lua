6f5c620fb414b52b963796d9d870a3f8fd0d0e9c
SRYBYOU <bloskmango@gmail.com>
2025-06-02 16:46:20 +0300

    Update UsersM.lua

--- FBI/UsersM.lua
+++ FBI/UsersM.lua
@@ -21,7 +21,7 @@ local BU = {
   
   PremiumMembers = {
     ["Hdjsisjsbsv"] = {
-        expirationDate = os.time{year = 2020, month = 6, day = 10}
+        expirationDate = os.time{year = 2025, month = 6, day = 10}
     }
   }, 
 
