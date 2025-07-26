bf2403b1a93574acf1d7086497dcfaf9c35fc93b
No Name.. <bloskmango@gmail.com>
2025-07-26 11:12:35 +0300

    Create BlockedWords.lua

---
+++ FBI/BlockedWords.lua
@@ -0,0 +1,31 @@
+return {
+	BlockedWords = {
+		["Dev"] = true,
+		["Owner"] = true,
+		["Admin"] = true,
+		["Premium"] = true,
+		["VIP"] = true,
+		["Distinct"] = true,
+		["( Expired )"] = true,
+		["Permanent"] = true,
+		["Working"] = true,
+		["Maintenance"] = true,
+		["Safe"] = true,
+		["Risk"] = true,
+		["Middle"] = true
+	},
+
+	BlockedArabWords = {
+		["مطور"] = true,
+		["مالك"] = true,
+		["ادمن"] = true,
+		["مميز"] = true,
+		["منتهي"] = true,
+		["دائم"] = true,
+		["شغال"] = true,
+		["صيانة"] = true,
+		["آمن"] = true,
+		["خطر"] = true,
+		["متوسط"] = true
+	}
+}
